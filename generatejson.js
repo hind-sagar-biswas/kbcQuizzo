d2417441f2757c235875872c87aa1671051a098d
Hind Sagar Biswas <hindsbhk@gmail.com>
2021-03-01 20:05:09 +0600

    Add files via upload

--- generatejson.js
+++ generatejson.js
@@ -1,8 +1,57 @@
 var jsonCode;
-
+var validSubCatagories = {
+	science : [
+		"chemistry",
+		"chem",
+		"physics",
+		"phy",
+		"biology",
+		"bio"
+	],
+	ict : [
+		"software",
+		"website",
+		"web",
+		"programming",
+		"prog",
+		"security"
+	],
+	literature : [
+		"bangla",
+		"bng",
+		"english",
+		"eng",
+		"hindi",
+		"hnd"
+	],
+	grammer : [
+		"bangla",
+		"bng",
+		"english",
+		"eng",
+		"hindi",
+		"hnd"
+	],
+	history : [
+		"international",
+		"bangladesh",
+		"india"
+	],
+	mathematics : [
+		"general",
+		"higher"
+	],
+	generalKnowledge : [
+		"null",
+		""
+	]
+}
 
 const form = document.forms["generator"];
 const questionInput = form["question"];
+const categoryInput = form["category"];
+const subCategoryInput = form["subCategory"];
+const difficultyInput = form["difficulty"];
 const answerInput = form["answer"];
 const optionInput = form["options"];
 const output = document.getElementById("output");
@@ -12,12 +61,23 @@ function generateCode() {
 	var question = questionInput.value.trim();
 	var answer = answerInput.value.trim();
 	var options = optionInput.value.split(";");
+	var category = categoryInput.value;
+	var subCategory = subCategoryInput.value.trim();
+	var difficulty = difficultyInput.value;
+	
+	if(!validSubCatagories[category].includes(subCategory)) {
+		alert(`"${subCategory}" is not a valid Subcategory!`);
+		subCategoryInput.value = "";
+		return false;
+	}
 	
 	for(var i = 0; i < 3; i++){
 		options[i] = options[i].trim();
 	}
 	
 	questionInput.value = "";
+	categoryInput.value = "";
+	subCategoryInput.value = "";
 	answerInput.value = "";
 	optionInput.value = "";
 	
@@ -27,6 +87,9 @@ function generateCode() {
 	
 	jsonCode += `		{
 			"question" : "${question}",
+			"category" : "${category}",
+			"subCategory" : "${subCategory}",
+			"difficulty" : "${difficulty}",
 			"answer" : "${answer}",
 			"otherOptions" : [
 				"${options[0]}",
